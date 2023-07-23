f9e77f0e928cd08cd4644fb0b12a9141c2a23326
cdr3xl3r <126210495+cdr3xl3r@users.noreply.github.com>
2023-07-23 10:29:12 -0400

    main

---
+++ Config/main.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import dotenv_values
+
+config = {
+    **dotenv_values(".env.shared"),  # load shared development variables
+    **dotenv_values(".env.secret"),  # load sensitive variables
+    **os.environ,  # override loaded values with environment variables
+}
+def main(args=None):
+    
