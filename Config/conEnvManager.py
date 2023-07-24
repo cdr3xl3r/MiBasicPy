d3e729a5dabb2dbb9dfd91175a3920b23c83f0a3
cdr3xl3r <126210495+cdr3xl3r@users.noreply.github.com>
2023-07-24 18:02:05 -0400

    MOREeNV

--- Config/conEnvManager.py
+++ Config/conEnvManager.py
@@ -1,3 +1,9 @@
-import datetime as dt 
-import os 
-from dotenv import load_dotenv
+import os
+from dotenv import dotenv_values
+
+config = {
+    **dotenv_values('..\\MiBasicPy\\Config\\Env\\.secret.env'),
+    **dotenv_values('..\\MiBasicPy\\Config\\Env\\.shared.env'),
+    **os.environ
+}
+print(config['test']+config['tests'])
